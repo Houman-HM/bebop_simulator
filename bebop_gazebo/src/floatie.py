ae36c20be965db302ed2d56468b5f437fe070045
Houman-HM <hm.houman@gmail.com>
2022-01-22 19:14:00 +0200

    bebop_gazebo original package

--- bebop_gazebo/src/floatie.py
+++ bebop_gazebo/src/floatie.py
@@ -4,7 +4,7 @@ from geometry_msgs.msg import Twist
 
 def init():
     rospy.init_node('bebop_hover')
-    twist_pub = rospy.Publisher("hover/cmd_vel", Twist, queue_size=1);
+    twist_pub = rospy.Publisher("bebop_hover/cmd_vel", Twist, queue_size=1);
     while not rospy.is_shutdown():
         twist_pub.publish(Twist())
         rospy.sleep(0.4)
